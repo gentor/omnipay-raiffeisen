8d677f3e700ba97d5377e1ddc04d43166f2a2ba8
Evgeni Nurkov <evgeni.nurkov@gmail.com>
2023-02-07 18:03:09 +0200

    Change $liveEndpoint and $testEndpoint

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -9,8 +9,8 @@ use Omnipay\Raiffeisen\Signature;
 
 abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
 {
-    protected $liveEndpoint = 'https://secure.rbb.bg/rbbg';
-    protected $testEndpoint = 'https://ecg.test.upc.ua/rbbg';
+    protected $liveEndpoint = 'https://secure.kbcbank.bg/kbc';
+    protected $testEndpoint = 'https://ecg.test.upc.ua/kbc/pay';
 
     /**
      * @inheritDoc
